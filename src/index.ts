249a8412481262d35a42bc8534549a6387d7aad1
liuyuyang <2281487673@qq.com>
2024-10-21 18:00:29 +0800

    修复了localStorage报错问题

--- src/index.ts
+++ src/index.ts
@@ -27,9 +27,9 @@ export function createMapperHooksStore<Result = unknown, Params = unknown>(
 ): HooksStoreType<Result, Params> | HooksStorePureType<Result, Params> {
     const withLocalStorage = options?.withLocalStorage ?? '';
     let curValue = initValue;
-    const local = getLocalObject(options?.isReactNative ? 'ReactNative' : 'web');
-    if (typeof local !== 'undefined' && withLocalStorage !== '') {
-        const token = !local.getItem(withLocalStorage) ? '{"value": "","type": "other"}' : local.getItem(withLocalStorage);
+    const local = getLocalObject(options?.local);
+    if (withLocalStorage !== '' && local != null) {
+        const token = !local?.getItem(withLocalStorage) ? '{"value": "","type": "other"}' : local?.getItem(withLocalStorage);
         const obj = JSON.parse(token as string);
         curValue = getChangeType(obj.type, obj.value) ?? undefined;
     }
@@ -178,12 +178,12 @@ export function createMapperHooksStore<Result = unknown, Params = unknown>(
                 throw new Error(error);
             }
         }
-        if (typeof local !== 'undefined' && withLocalStorage !== '') {
+        if (withLocalStorage !== '' && local) {
             const obj = {
                 value: typeof ref.value === 'string' ? ref.value : JSON.stringify(ref.value),
                 type: typeof ref.value,
             };
-            local.setItem(`${withLocalStorage}`, JSON.stringify(obj));
+            local?.setItem(`${withLocalStorage}`, JSON.stringify(obj));
         }
         private_emit();
     }
--- src/utils/__test__/local.test.ts
+++ src/utils/__test__/local.test.ts
@@ -1,15 +1,28 @@
 // local.test.ts
-import AsyncStorage from '@react-native-async-storage/async-storage';
 import {getLocalObject} from '../local';
 
 describe('getLocalObject function', () => {
     test('should return AsyncStorage when env is ReactNative', () => {
-        const result = getLocalObject('ReactNative');
-        expect(result).toBe(AsyncStorage);
+        const result = getLocalObject(localStorage);
+        expect(result).toBe(localStorage);
     });
 
     test('should return localStorage when env is not ReactNative', () => {
         const result = getLocalObject();
         expect(result).toBe(localStorage);
+    });
+
+    test('should return null when env is not ReactNative and local is undefined', () => {
+        const result = getLocalObject({local: undefined});
+        // 期望的应该是{local: undefined}
+        expect(result).toEqual({local: undefined});
+    });
+
+    test('should return null', () => {
+        // @ts-expect-error
+        // eslint-disable-next-line no-global-assign
+        delete globalThis.localStorage;
+        const result = getLocalObject();
+        expect(result).toEqual(null);
     });
 });
